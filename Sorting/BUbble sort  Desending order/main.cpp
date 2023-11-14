5f743ff19116907ea2df82690d53c22367603af9
susmita413 <137711194+susmita413@users.noreply.github.com>
2023-11-14 23:10:22 +0600

    Add files via upload

---
+++ Sorting/BUbble sort  Desending order/main.cpp
@@ -0,0 +1,58 @@
+# include <iostream>
+
+using namespace std;
+
+
+void bubbleSortDesending(int a[],int n){
+
+    int temp ;
+
+    for(int i=0; i<n-1; i++){
+
+        int flag = 0;
+
+        for(int j=0 ; j<n-1-i; j++){
+
+            if(a[j] < a[j+1]){
+                temp= a[j];
+                a[j] = a[j+1];
+                a[j+1] = temp;
+                flag=1;
+
+                cout<< "temp= " <<temp << ", i= " << i << ", j= " << j << endl ;
+            }
+        }
+
+        
+        cout << " i= " << i  << endl ;
+
+
+        if(flag== 0){
+            break ;
+        }
+    }
+}
+int main(){
+
+    int arr[]= {1,2,3,4};
+    int len = sizeof(arr)/sizeof(arr[0]);
+
+    cout << endl << " before sorting: " ;
+
+    for(int i=0; i<len; i++){
+        cout << arr[i] << " " ;
+    }
+
+    bubbleSortDesending(arr,len);
+
+
+    cout<< endl << " after Sorting DESENDING : " ;
+
+    for(int i=0; i<len; i++){
+        cout << " " << arr[i]  ;
+    }
+
+
+    return 0;
+
+}
---
+++ Sorting/Bubble sort ascending order/main.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+
+using namespace std;
+
+void bubbleSort(int a[], int n){
+
+    int temp ;
+
+
+    for(int i=0; i<n-1 ; i++){
+
+        int flag=0 ;
+
+        for(int j=0; j<n-1-i; j++){
+
+            if(a[j]> a[j+1]){
+                temp= a[j];
+                a[j] = a[j+1];
+                a[j+1]= temp;
+                cout << endl <<"temp= " << temp << ", i= " << i << ", j= " << j << endl ;
+            
+               flag=1;
+            }
+        }
+
+
+        cout << endl <<"after every inner LOOP: temp= " << temp << ", i= " << i << endl ;
+
+
+        if(flag==0 ){
+            break ;
+        }
+
+
+       
+    }
+}
+
+
+int main()
+{
+    int arr[]= {5,4,3,2,1};
+    int len= sizeof(arr)/sizeof(arr[0]);
+
+    cout << "before sorting: " ;
+
+    for(int i=0; i<len; i++){
+        cout << arr[i] << " " ;
+    }
+
+    bubbleSort(arr,len);
+
+    cout << endl << " After sorting: " ;
+
+    for(int i=0; i<len; i++){
+        cout << arr[i] << " " ;
+    }
+
+    cout << endl << "Hello world!" << endl;
+    return 0;
+
+
+
+}
